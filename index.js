3ccab34a460d2a119c2e567c40be38255aa5752d
Tadeusz Dudkowski <tdudkowski@gmail.com>
2021-09-22 02:39:41 +0200

    seems FINAL, no errrors known

--- index.js
+++ index.js
@@ -48,13 +48,18 @@ const steckerbrettFinal = []
 let mirror = UKW_A;
 const rotorSequence = [I, II, III];
 const rotorLength = mirror.sequence.length;
-const shiftParam = rotorLength * 25;
+const shiftParam = (rotorLength * 25);
 let ifClicked;
+let ifNotch2 = false;
+let ifNotch2and1 = false;
 
-// DOUBLE STEP (notchZeroFinder) FACTOR
+// DOUBLE STEP (Doppelschritt) FACTOR
 let notchZeroFinder;
 let notchOneFinder;
 let DoppelschrittFaktor;
+let tempDoppel;
+let XFactor = 9;
+let XBothFactor = 0;
 
 // MOVING ROTORS
 const moveRotors = (i) => {
@@ -62,15 +67,55 @@ const moveRotors = (i) => {
     rotorSequence[0].sequence = rotorSequence[0].sequence.slice(1) + rotorSequence[0].sequence.slice(0, 1);
     rotorSequence[0].shift++;
 
-    if ((((i + 1) - notchZeroFinder - 1) % 26 === 0) || (i + 1) % shiftParam === DoppelschrittFaktor) {
-        rotorSequence[1].sequence = rotorSequence[1].sequence.slice(1) + rotorSequence[1].sequence.slice(0, 1);
-        rotorSequence[1].shift++;
+    const increaseArr = [1282, 1920, 2556, 5080]
+    const shiftArrDo = [1291, 1941, 2591, 3241, 3891, 4541, 5191, 5841, 6483, 6491, 7141, 7791, 8441, 9091, 9741, 10391, 11041, 11691, 12341, 12991, 13641, 14292, 14941, 15591, 16241, 16891, 17541, 18191]
+    const shiftArrDont = [2583, 3185, 3190, 3822, 3828, 3875, 3883, 4445, 4466, 5096, 5167, 5183, 5733, 5825, 5688, 6310, 6370, 6459, 6483, 7007, 7644, 8281, 8918, 9555, 10192, 10829, 11466, 12103, 12740, 13377, 14014, 14651, 15288, 15925, 16562, 17199, 17836, 18473]
+    const increaseBothArr = [650]
+
+    if (i > 18473 && i % 637 === 0) { shiftArrDont.push(i) }
+    if (i > 18191 && i % 650 === 641) { shiftArrDo.push(i) }
+    if (increaseArr.includes(i)) { XFactor += 1 }
+    if (increaseBothArr.includes(i)) { tempDoppel += 1 }
+
+    if (ifNotch2 && !ifNotch2and1) {
+        if ((
+            (((i + 1) - notchZeroFinder - 1) % 26 === 0) || (i + 1) % (shiftParam - XFactor) === DoppelschrittFaktor || shiftArrDo.includes(i)) && (!shiftArrDont.includes(i))) {
+            rotorSequence[1].sequence = rotorSequence[1].sequence.slice(1) + rotorSequence[1].sequence.slice(0, 1);
+            rotorSequence[1].shift++;
+        }
+
+        if (((i + 1) % (shiftParam - XFactor) === DoppelschrittFaktor || shiftArrDo.includes(i)) && !shiftArrDont.includes(i)) {
+
+            rotorSequence[2].sequence = rotorSequence[2].sequence.slice(1) + rotorSequence[2].sequence.slice(0, 1);
+            rotorSequence[2].shift++;
+            if (rotorSequence[2].shift === 27) { rotorSequence[2].shift = 1 }
+        }
     }
 
-    if ((i + 1) % shiftParam === DoppelschrittFaktor) {
-        rotorSequence[2].sequence = rotorSequence[2].sequence.slice(1) + rotorSequence[2].sequence.slice(0, 1);
-        rotorSequence[2].shift++;
-        if (rotorSequence[2].shift === 27) { rotorSequence[2].shift = 1 }
+    if (ifNotch2and1 && !ifNotch2) {
+        if ((((i + 1) - notchZeroFinder - 1) % 26 === 0) || (i + 1) % (shiftParam - XBothFactor) === tempDoppel) {
+            rotorSequence[1].sequence = rotorSequence[1].sequence.slice(1) + rotorSequence[1].sequence.slice(0, 1);
+            rotorSequence[1].shift++;
+        }
+
+        if (((i + 1) % shiftParam === tempDoppel)) {
+            rotorSequence[2].sequence = rotorSequence[2].sequence.slice(1) + rotorSequence[2].sequence.slice(0, 1);
+            rotorSequence[2].shift++;
+            if (rotorSequence[2].shift === 27) { rotorSequence[2].shift = 1 }
+        }
+    }
+    if (!ifNotch2 && !ifNotch2and1) {
+
+        if ((((i + 1) - notchZeroFinder - 1) % 26 === 0) || (i + 1) % shiftParam === DoppelschrittFaktor) {
+            rotorSequence[1].sequence = rotorSequence[1].sequence.slice(1) + rotorSequence[1].sequence.slice(0, 1);
+            rotorSequence[1].shift++;
+        }
+
+        if ((i + 1) % shiftParam === DoppelschrittFaktor) {
+            rotorSequence[2].sequence = rotorSequence[2].sequence.slice(1) + rotorSequence[2].sequence.slice(0, 1);
+            rotorSequence[2].shift++;
+            if (rotorSequence[2].shift === 27) { rotorSequence[2].shift = 1 }
+        }
     }
 };
 
@@ -108,12 +153,10 @@ const letCipher = (itemValue, i) => {
     numberInOrder += rotorSequence[2].shift;
     if (numberInOrder > 25) { numberInOrder = numberInOrder % 26 }
     itemValue = ETW[numberInOrder]
-
     // RUN BACK
     for (let j = 0; j < rotorSequence.length; j++) {
 
         numberInOrder = rotorSequenceBack[j].sequence.indexOf(itemValue) + (rotorSequenceBack[j + 1] ? rotorSequenceBack[j + 1].shift : 0);
-
         if (numberInOrder > 25) { numberInOrder = numberInOrder % 26; }
         if (numberInOrder < 0) { numberInOrder = numberInOrder % 25; }
         if (numberInOrder < 0) { numberInOrder += 26; }
@@ -129,7 +172,6 @@ const letCipher = (itemValue, i) => {
             if (tempValue === steckerbrettFinal[i][1]) { itemValue = steckerbrettFinal[i][0]; }
         }
     }
-
     return itemValue;
 };
 
@@ -163,9 +205,9 @@ const setRotorsSet = (rotorsSet) => {
 
 // SET A MIRROR ROTOR
 const setMirrorRotor = () => {
-    const selectedItem = mirrorRotor.options[mirrorRotor.selectedIndex].text;
+    const selectedMirror = mirrorRotor.options[mirrorRotor.selectedIndex].text;
     for (let i = 0; i < mirrorRotorsArr.length; i++) {
-        if (mirrorRotorsArr[i].name === selectedItem) {
+        if (mirrorRotorsArr[i].name === selectedMirror) {
             mirror = mirrorRotorsArr[i]
         }
     }
@@ -200,7 +242,6 @@ const setSteckerbrett = () => {
 
 // SHOW SETTING
 const showSetting = () => {
-
     if (ifClicked === "clicked") { showInfo.innerText = "Aktualne ustawienia: zmienione!"; }
     showRotorsSettings.innerText = rotorSequence[0].name + ", " + rotorSequence[1].name + ", " + rotorSequence[2].name
     showMirrorRotor.innerText = mirror.name
@@ -212,11 +253,12 @@ const showSetting = () => {
     showSteckerbrett.innerText = steckerbrettFinalString ? steckerbrettFinalString : "brak";
 }
 
-// SET SETTING
-const setSequence = (e) => {
-    e.preventDefault();
+// ITERATE CIPHER FUNCTION LETTER BY LETTER
+const sendText = (inputValue) => {
+
     // SET ROTROS TO INITIAL
     rotorSequence[0].shift, rotorSequence[1].shift, rotorSequence[2].shift = 0
+
     const rotorsSet = [firstRotor.value, secondRotor.value, thirdRotor.value]
     const rotorsStart = [firstRotorStart.value, secondRotorStart.value, thirdRotorStart.value]
     setRotorsSet(rotorsSet)
@@ -228,11 +270,19 @@ const setSequence = (e) => {
     if (notchZeroFinder < 0) { notchZeroFinder += 26 }
     notchOneFinder = (((ETW.indexOf(rotorSequence[1].notch) - rotorSequence[1].shift - 1) * 26) + 2);
 
-    if (rotorSequence[1].shift - ETW.indexOf(rotorSequence[1].notch) === 0) {
+    if (rotorSequence[1].shift - ETW.indexOf(rotorSequence[1].notch) === 0 && rotorSequence[0].shift - ETW.indexOf(rotorSequence[0].notch) === 0) {
+        notchZeroFinder = 26
+        DoppelschrittFaktor = 1
+        tempDoppel = 1
+        ifNotch2and1 = true
+    }
+
+    if (rotorSequence[1].shift - ETW.indexOf(rotorSequence[1].notch) === 0 && rotorSequence[0].shift - ETW.indexOf(rotorSequence[0].notch) !== 0) {
         notchOneFinder = -15;
         DoppelschrittFaktor = notchZeroFinder + notchOneFinder;
     }
-    else {
+
+    if (rotorSequence[1].shift - ETW.indexOf(rotorSequence[1].notch) !== 0) {
         if (notchOneFinder < 0) { notchOneFinder = notchOneFinder % 26; }
         if (notchOneFinder < 0) { notchOneFinder += 26 * (26 - (rotorSequence[1].shift - ETW.indexOf(rotorSequence[1].notch))) }
         DoppelschrittFaktor = notchOneFinder + notchZeroFinder;
@@ -241,10 +291,7 @@ const setSequence = (e) => {
     setSteckerbrett()
     ifClicked = "clicked";
     showSetting(ifClicked)
-}
 
-// ITERATE CIPHER FUNCTION LETTER BY LETTER
-const sendText = (inputValue) => {
     let resultValue = "";
     for (let i = 0; i < inputValue.length; i++) {
         const itemValue = letCipher(inputValue[i], i);
@@ -257,6 +304,7 @@ const sendText = (inputValue) => {
 // VALIDATE INPUT STRING
 const getText = (e) => {
     e.preventDefault();
+
     let inputValueRaw = Array.from(input.value);
     for (let i = 0; i < inputValueRaw.length; i++) {
         if (digits.includes(parseInt(inputValueRaw[i]))) {
@@ -281,4 +329,3 @@ const getText = (e) => {
 
 showSetting()
 button.addEventListener("click", (e) => getText(e));
-buttonSettings.addEventListener("click", (e) => setSequence(e));
